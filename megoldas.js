9414db971e013c49f82011d4b81e92c28bd3cce0
CsillaKarpati <90130061+CsiMariposa@users.noreply.github.com>
2023-02-04 18:53:33 +0100

    Beküldendő230204

--- megoldas.js
+++ megoldas.js
@@ -1,81 +1,146 @@
+<script>
+//1.feladat
+/*1. keszito() – Bemeneti paraméterek száma: 0
+Készíts egy függvényt mely kiírja az adott file készítőjének 
+• Nevét
+• Csoportjának típusát (Junior Frontend vagy Webfejlesztő) 
+• Csoportjának azonosítóját (melyik #Team tagja)
+Visszatérési érték: nincs
+*/
+function keszito() {
+	document.write("A készítő neve: Kárpáti Csilla <br>"); 
+    	document.write("A csoport típusa: Junior FrontEnd és Webfejlesztő <br>"); 
+	document.write("A csoport azonosítója: team12 <br>"); 
+}
+keszito(); 
 
-//1.feladat file készítőjének nevét írja ki
-document.write(" Kárpáti Csilla, #TEAM tagja <br>"); 
-document.write(" HTML-t 90 ig érti <br>"); 
-document.write(" CSS-t 80 ig érti <br>"); 
-document.write(" Javascripttel még küzd. leginkább a matek miatt, sorry: 30 <br>")
-document.write("<hr>");
+//2.feladat
+/*
+2. feladat: hatvanyozo() – Bemeneti paraméterek száma: 2 (szam és hatvány)
+Készíts egy függvényt, mely paraméterként bekér egy számot és a hatványozás mértékét, és 
+visszaadja annak hatványát.
+Kipróbálásra: hatvanyozo(2,3) esetleg hatvanyozo(5,3)
+Visszatérési érték: 8, illetve a második esetében 125
+*/
+/*//prompt bekérésnél nem íródik ki- ezt nem tudom, hogyan kellene 
+function hatvanyozo(szam,hatvany) {
+    let eredmeny=Math.pow(szam,hatvany);
+    document.write("A hatvány értéke: "+eredmeny+"<br>"); 
+    return eredmeny; 
+}
+let szam=prompt("Adj meg egy számot:" );
+let hatvany=prompt("Add meg a szám hatványát:");
+hatvanyozo(); */
 
-//2. feladatBekérünk egy számmot és hatványozzuk
-let x=Number(prompt("Adj meg egy számot:"));
-let y=Number(prompt("Adja meg a hatvány értéket:"));
-let z=(x**y); 
- document.write(`A szám hatványa: ${z}`);
- document.write("<hr>");
- 
-//3.feladat - adott értékben generálás
-let a=Number(prompt("Adj meg egy kezdőértéket:"));
-let b=Number(prompt("Adja meg egy végső értéket:"));
-let randomSzam=(Math.round(Math.random()*a))+b; 
- document.write(randomSzam);
-if(randomSzam%2==0){
-	document.write(`A 3.feladat: A ${randomSzam}`);
+//Ha nem kell bekérni akkor: 
+function hatvanyozo(szam1,hatvany1) {
+    let eredmeny1=Math.pow(szam1,hatvany1);
+    document.write("A hatvány értéke: "+eredmeny1+"<br>"); 
+    return eredmeny1; 
+}
+hatvanyozo(2,3); 
+hatvanyozo(5,3); 
+
+/*3.feladat
+3. feladat: parosLetrehoz() – Bemeneti paraméterek száma: 2 (alsó és felső határ)
+Készíts egy függvényt, ami egy adott intervallumban generál ki páros számot, és ad vissza 
+értéknek. 
+Kipróbálásra: parosLetrehoz(1,100);
+Visszatérési érték: 2-100-ig bármely páros szám
+*/
+
+
+function parosLetrehoz(also, felso) {
+let ParosSzam=Math.round(Math.random()*(felso-also))+also;
+if(ParosSzam%2==0){
+	document.write(`Generált páros szám a(z) ${also}-${felso} intervallumban: ${ParosSzam}<br>`);
+}
+else if(ParosSzam!=felso)
+{
+	document.write(`Generált páros szám a(z) ${also}-${felso} intervallumban: ${ParosSzam+1}<br>`);
 }
 else{
-	document.write(" ");
+	document.write(`Generált páros szám a(z) ${also}-${felso} intervallumban: ${ParosSzam-1} <br>`);
 }
-document.write("<hr>");
+}
 
-//4.feladat Élekor program, ami bekér 1-120 ig. 
-let eletkor=Number(prompt("Add meg az életkorát: "));
+parosLetrehoz(1,100); 
 
-if(eletkor<6){
-	document.write(`A megadott életkor alapján Ön életkora: ${eletkor}, kisgyermekkorban van`);
-}
-else if(eletkor<12){
-	document.write(`A megadott életkor alapján Ön életkora: ${eletkor}, gyermekkorban van`);
-}
-else if(eletkor<16){
-	document.write(`A megadott életkor alapján Ön életkora: ${eletkor}, serdülőkorban van`);
-}
-else if(eletkor<20){
-	document.write(`A megadott életkor alapján Ön életkora: ${eletkor}, ifjúkorban`);
-}
-else if(eletkor<30){
-	document.write(`A megadott életkor alapján Ön életkora: ${eletkor}, fiatal felnőtt kor`);
-}
-else if(eletkor<60){
-	document.write(`A megadott életkor alapján Ön életkora: ${eletkor}, felnőtt korban van`);
-}
-else if(eletkor<120){
-	document.write(`A megadott életkor alapján Ön életkora: ${eletkor}, aggkorban van`);
-}
+/*4.feladat 
+4. feladat: testTomegIndex() – Bemeneti paraméterek száma: 2 (suly és magasság)
+Készíts el egy függvényt, amelynek meg kell adnod a magasságodat méterben és a súlyodat 
+kilogrammban, és ezek után visszaadja a jelenlegi testtömeg indexed rövid szöveges 
+értékelését.
+A testtömeg index kiszámítására, a mellékelt linken találhatsz forrást:
+Forrás: https://hu.wikipedia.org/wiki/Testt%C3%B6megindex
+Visszatérési értékek lehetőségei (ügyelj a kis-nagybetű érzékenységre!):
+• sulyosSovanysag
+• mersekeltSovanysag
+• enyheSovanysag
+• normalisTestsuly
+• tulsulyos
+• IfokuElhizas
+• IIfokuElhizas
+• IIIfokuSulyosElhizas
+Kipróbálásra: testTomegIndex(200,2) esetleg testTomegIndex(45,1.5), 
+testTomegIndex(25,1.2)
+Visszatérési érték: IIIfokuSulyosElhizas illetve normalisTestsuly valamint enyheSovanysag
+*/
+
+function testTomegIndex(testtomeg, magassag) {
+	let TTI=testtomeg/(magassag*magassag);
+	if(TTI<16){
+	document.write("Magasságod: "+magassag+", súlyod: "+testtomeg+"<br> Súlyos soványság <br>");
+	}
+else if(TTI<17){
+	document.write(" Magasságod: "+magassag+", súlyod: "+testtomeg+"<br> Mérsékelt soványság <br>");
+	}
+else if(TTI<18.5){
+	document.write(" Magasságod: "+magassag+", súlyod: "+testtomeg+"<br>Enyhe soványság <br>");
+	}
+else if(TTI<25){
+	document.write( "Magasságod: "+magassag+", súlyod: "+testtomeg+" <br>Normális testsúly <br>");
+	}
+else if(TTI<30){
+	document.write("Magasságod: "+magassag+", súlyod: "+testtomeg+" <br>Túlsúlyosság <br>");
+	}
+else if(TTI<35){
+	document.write("Magasságod: "+magassag+", súlyod: "+testtomeg+"<br>I. fokú elhízás <br>");
+	}
+else if(TTI<40){
+	document.write("Magasságod: "+magassag+", súlyod: "+testtomeg+"<br>II. fokú elhízás <br>");
+	}
 else {
-	document.write("Kérem adja meg az életkorát 0-120 között");
+	document.write("Magasságod: "+magassag+", súlyod: "+testtomeg+"<br>III. fokú (súlyos) elhízás <br>");
+	}
+    return TTI; 
 }
-document.write("<hr>");
 
+testTomegIndex(200,2);  
+testTomegIndex(45,1.5); 
+testTomegIndex(25,1.2); 
 
+/*5.feladat 
+5. feladat: egeszOsztoE() – Bemeneti paraméterek száma: 2 (szam és osztó)
+Készíts egy függvényt, mely paraméterként bekér egy számot és egy osztót és vissza adja(true 
+vagy false), hogy az adott osztó, osztja-e az egész számot, úgy, hogy a maradék nulla.
+Kipróbálásra: egeszOsztoE(25,5) esetleg egeszOsztoE(1050,7) valamint egeszOsztoE(2048,3)
+Visszatérési érték: true, illetve a második esetében szintén true, harmadik esetén pedig false*/
 
-//5.feladat: Készíts egy olyan kódot, mely paraméterként bekér egy számot és egy osztót és
-//kiírjaszövegesen,hogy az adott osztó, osztja-e az egész számot, úgy, hogy a maradék nulla
-let c=prompt("Adj meg egy számot:");
-let e=prompt("Adj meg egy osztót:");
-let f=c%e; 
-document.write("5.feladat");
-if (f==0){
-	document.write(`Az adott osztó osztja az egész számot, úgy, hogy a maradék nulla ${f} <br>` );
+function egeszOsztoE(szam, oszto) {
+if(szam%oszto==0){
+	document.write(`A ${oszto} osztója ${szam} értéknek <br>`);
+    return true; 
 }
 else{
-	document.write(`Az adott osztó nem osztja az egész számot, úgy, hogy a maradék nulla legyen ${f} <br>`);
+	document.write(`A ${oszto} NEM osztója ${szam} értéknek <br>`);
+    return false; 
 }
 
-//6. feladat: Készíts egy programot,ami kiírja az első 10 négyzetszámot!
-document.write("<hr>");
-let n=10; 
-let k=1;
-for(let i=1; i<=n; i++){
-    k=i*i; 
-	document.write(k+",");
-    
 }
+//let szam=Number(prompt("Adj meg egy számot:"));
+//let oszto=Number(prompt("Adj meg egy osztót:"));
+egeszOsztoE(25,5); 
+egeszOsztoE(1050,7); 
+egeszOsztoE(2048,3); 
+</script>
